3168f74c9d83c28eebe0c9fa8c2e80c7e1ef7c8f
Ekode <ekode@protonmail.com>
2020-07-30 15:15:20 +0200

    Added first interfaces

---
+++ src/main/java/Controllers/ControllerAdminAcceuil.java
@@ -0,0 +1,24 @@
+package Controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class ControllerAdminAcceuil {
+    @FXML
+    Button btn_logout;
+    Button btn_student_edit;
+    Button btn_student_add;
+    Button btn_student_delete;
+    Button btn_student_list;
+    Button btn_class_create;
+    Button btn_class_edit;
+    Button btn_class_delete;
+
+    @FXML
+    public void initialize(){
+
+    }
+
+}
---
+++ src/main/java/Controllers/ControllerAdminClassAdd.java
@@ -0,0 +1,19 @@
+package Controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class ControllerAdminClassAdd {
+
+    @FXML
+    Button btn_logout;
+    Button btn_confirm;
+    Button btn_cancel;
+    TextField txt_class_name;
+
+    @FXML
+    public void initialize(){
+
+    }
+}
