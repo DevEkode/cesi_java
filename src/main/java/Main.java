20a2c3a9b6ef9fadd41c4493fcfeadb4071fb4b1
guillaumemb <guillaume.mbali72@gmail.com>
2020-07-30 16:15:43 +0200

    Database management

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,8 +3,38 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import java.io.IOException;
+import java.sql.SQLException;
 
 public class Main extends Application {
+
+    public static void main(String[] args) throws SQLException {
+
+
+        try {
+            BddConnexion maCo = new BddConnexion("C:\\Users\\guill\\IdeaProjects\\cesi_java\\src\\main\\resources\\db.properties");
+            maCo.seConnecter();
+            Classroom classroom = new Classroom(maCo);
+            Person person = new Person(maCo);
+            Role role = new Role(maCo);
+
+           // role.insertRole("administratrice");
+
+           // role.showRole();
+            System.out.println("------------------------------------------");
+            classroom.DeleteClassroom(2);
+            classroom.showClassroom();
+            //System.out.println("------------------------------------------");
+            //maCo.eraseAll();
+            //person.showClassroom();
+            //System.out.println("------------------------------------------");
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
 
     @Override
     public void start(Stage primaryStage) throws Exception{
@@ -15,7 +45,4 @@ public class Main extends Application {
     }
 
 
-    public static void main(String[] args) {
-        launch(args);
-    }
 }
