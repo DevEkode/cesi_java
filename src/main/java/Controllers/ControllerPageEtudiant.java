cbfd78315f621fc99be08f11933e2aa80a1b416c
Mansour <mansour.ozda@gmail.com>
2020-07-31 16:51:39 +0200

    Ajout des nom / Prenom et Promotion de l'application ( Page étudiant )

--- src/main/java/Controllers/ControllerPageEtudiant.java
+++ src/main/java/Controllers/ControllerPageEtudiant.java
@@ -1,7 +1,11 @@
 package Controllers;
 
+
 import BDD.BddConnexion;
+import BDD.Classroom;
 import BDD.Person;
+import Enums.RoleId;
+import ListObjects.PersonItem;
 import Models.Model_pageEtudiant;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -16,8 +20,12 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class ControllerPageEtudiant {
 
@@ -33,23 +41,51 @@ public class ControllerPageEtudiant {
     Text Txt_promotion;
     @FXML
     Button btn_logout;
-    TableView<Model_pageEtudiant> Table_liste_etudiants;
-    TableColumn<Model_pageEtudiant,String> col_prenom;
-    TableColumn<Model_pageEtudiant,String> col_nom;
-    TableColumn<Model_pageEtudiant,String> col_classe;
+    @FXML
+    ListView<Model_pageEtudiant> liste_etudiants;
+    private Classroom classroom;
 
-    ObservableList<Model_pageEtudiant> oblist = FXCollections.observableArrayList();
 
     public void initialize() throws IOException, SQLException {
+        ArrayList<Model_pageEtudiant> oblist =  new ArrayList<>();
 
-        BddConnexion bddConnexion = BddConnexion.getINSTANCE();
-        this.person = new Person(bddConnexion);
 
-        // ResultSet rs = Connection.createStatement().executeQuery("select firstname,lastname,classe FROM person ");
+        try {
+            BddConnexion bddConnexion = BddConnexion.getINSTANCE();
+            this.classroom = new Classroom(bddConnexion);
+            this.person = new Person(bddConnexion);
+            ResultSet rt = this.person.showPerson(personId);
+            int classroomId = -1;
+            while (rt.next()){
+                classroomId = rt.getInt("idClassroom");
+            }
 
-        col_prenom.setCellValueFactory(new PropertyValueFactory<>("prenom"));
-        col_nom.setCellValueFactory(new PropertyValueFactory<>("nom"));
-        col_classe.setCellValueFactory(new PropertyValueFactory<>("classe"));
+            ResultSet rs = this.person.showRolePersonClass(RoleId.ETUDIANT,classroomId);
+
+
+            while(rs.next()){
+                    oblist.add(new Model_pageEtudiant(rs.getString("firstname"),rs.getString("lastname")));
+            }
+
+        } catch(SQLException ex){
+
+            Logger.getLogger(ControllerListeEtudiant.class.getName()).log(Level.SEVERE,null,ex);
+        }
+
+
+            ObservableList<Model_pageEtudiant> maListeModel = FXCollections.observableList(oblist);
+            liste_etudiants.setItems(maListeModel);
+
+            ResultSet XT = this.person.showPerson(personId);
+
+            while(XT.next()){
+                String firstName = XT.getString("firstname");
+                String lastName =  XT.getString("lastname");
+                Txt_prenom.setText(firstName);
+                Txt_nom.setText(lastName);
+            }
+
+
 
     }
 
